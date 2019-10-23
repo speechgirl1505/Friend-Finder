7ea63cb978325dada81a6060c4cd58888a33da10
Kala Elam <speechgirl1505@gmail.com>
2019-10-23 17:35:17 -0500

    show me some pictures

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,14 +4,14 @@ var friendlyFolk = [
         "photo":"https://upload.wikimedia.org/wikipedia/en/d/dc/MichaelScott.png",
         "scores":[
             1,
+            2,
+            3,
             1,
+            2,
+            3,
             1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
+            3,
+            2,
             1,
 
           ]
@@ -19,125 +19,80 @@ var friendlyFolk = [
         "name":"Prison Mike",
         "photo":"https://vignette.wikia.nocookie.net/theoffice/images/9/96/Prisonmike.png/revision/latest?cb=20100327171549",
         "scores":[
+            1,
             2,
+            3,
+            4,
+            5,
+            5,
+            4,
+            3,
             2,
-            2,
-            2,
-            2,
-            2,
-            2,
-            2,
-            2,
-            2,
+            1,
           ]
       }
-      // , {
-      // "name":"Michael Klump",
-      //   "photo":"https://i.imgur.com/1I6QEtM.jpg",
-      //   "scores":[
-      //       3,
-      //       3,
-      //       3,
-      //       3,
-      //       3,
-      //       3,
-      //       3,
-      //       3,
-      //       3,
-      //       3,
-      //     ]
-      //   } 
-        // {
-        //     "name":"Caleb Crawdad",
-        //     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        //     "scores":[
-        //         5,
-        //         1,
-        //         4,
-        //         4,
-        //         5,
-        //         1,
-        //         2,
-        //         5,
-        //         4,
-        //         1
-        //       ] 
-        // }, {
-        //     "name":"McGruber",
-        // "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        // "scores":[
-        //     5,
-        //     1,
-        //     4,
-        //     4,
-        //     5,
-        //     1,
-        //     2,
-        //     5,
-        //     4,
-        //     1
-        //   ]
-        // }, {
-        //     "name":"Blind Guy McSqueezy",
-        // "photo":"",
-        // "scores":[
-        //     5,
-        //     1,
-        //     4,
-        //     4,
-        //     5,
-        //     1,
-        //     2,
-        //     5,
-        //     4,
-        //     1
-        //   ]
-        // }, {
-        //     "name":"Mykynos",
-        // "photo":"",
-        // "scores":[
-        //     5,
-        //     1,
-        //     4,
-        //     4,
-        //     5,
-        //     1,
-        //     2,
-        //     5,
-        //     4,
-        //     1
-        //   ]
-        // }, {
-        //     "name":"Disabled Mike",
-        //     "photo":"",
-        //     "scores":[
-        //         5,
-        //         1,
-        //         4,
-        //         4,
-        //         5,
-        //         1,
-        //         2,
-        //         5,
-        //         4,
-        //         1
-        //       ]
-        // }, {
-        //     "name":"Date Mike",
-        // "photo":"",
-        // "scores":[
-        //     5,
-        //     1,
-        //     4,
-        //     4,
-        //     5,
-        //     1,
-        //     2,
-        //     5,
-        //     4,
-        //     1
-        //   ]
-        // }
+      , {
+      "name":"Michael Klump",
+        "photo":"https://i.imgur.com/1I6QEtM.jpg",
+        "scores":[
+            4,
+            2,
+            4,
+            2,
+            4,
+            2,
+            4,
+            2,
+            4,
+            2,
+          ]
+        }, 
+        {
+            "name":"Date Mike",
+            "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+            "scores":[
+            1,
+            3,
+            5,
+            1,
+            3,
+            5,
+            1,
+            3,
+            5,
+            1
+            ] 
+        }, {
+            "name":"McGruber",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            1,
+            5,
+            1,
+            5,
+            1,
+            5,
+            1,
+            5,
+            5,
+            1
+          ]
+        }, {
+            "name":"Blind Guy McSqueezy",
+        "photo":"",
+        "scores":[
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
+          ]
+        }
   ];
   
 
