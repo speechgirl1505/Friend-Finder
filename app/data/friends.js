dbcc54afae8288113ee80d0ebd4a8efe87c87803
Kala Elam <speechgirl1505@gmail.com>
2019-10-24 08:27:20 -0500

    sneak peak

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friendlyFolk = [
     {
-        "name":"Michael",
-        "photo":"https://upload.wikimedia.org/wikipedia/en/d/dc/MichaelScott.png",
+        "name":"Michael Scott",
+        "photo":"https://media.giphy.com/media/xMGh0bajSyNdC/giphy.gif",
         "scores":[
             1,
             2,
@@ -16,69 +16,71 @@ var friendlyFolk = [
 
           ]
       }
-      // , {
-      //   "name":"Prison Mike",
-      //   "photo":"https://vignette.wikia.nocookie.net/theoffice/images/9/96/Prisonmike.png/revision/latest?cb=20100327171549",
-      //   "scores":[
-      //       1,
-      //       2,
-      //       3,
-      //       4,
-      //       5,
-      //       5,
-      //       4,
-      //       3,
-      //       2,
-      //       1,
-      //     ]
-      // }
-      // , {
-      // "name":"Michael Klump",
-      //   "photo":"https://i.imgur.com/1I6QEtM.jpg",
-      //   "scores":[
-      //       4,
-      //       2,
-      //       4,
-      //       2,
-      //       4,
-      //       2,
-      //       4,
-      //       2,
-      //       4,
-      //       2,
-      //     ]
-      //   }, 
-      //   {
-      //       "name":"Date Mike",
-      //       "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-      //       "scores":[
-      //       1,
-      //       3,
-      //       5,
-      //       1,
-      //       3,
-      //       5,
-      //       1,
-      //       3,
-      //       5,
-      //       1
-      //       ] 
-      //   }, {
-      //       "name":"McGruber",
-      //   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-      //   "scores":[
-      //       1,
-      //       5,
-      //       1,
-      //       5,
-      //       1,
-      //       5,
-      //       1,
-      //       5,
-      //       5,
-      //       1
-      //     ]
-      //   }, {
+      , {
+        "name":"Prison Mike",
+        "photo":"http://giphygifs.s3.amazonaws.com/media/aZeFIjI9hNcJ2/giphy.gif",
+        "scores":[
+            1,
+            2,
+            3,
+            4,
+            5,
+            5,
+            4,
+            3,
+            2,
+            1,
+          ]
+      }
+      , {
+      "name":"Michael Scarn",
+        "photo":"https://media.giphy.com/media/12MgUpnxEq3ypy/giphy.gif",
+        "scores":[
+            4,
+            2,
+            4,
+            2,
+            4,
+            2,
+            4,
+            2,
+            4,
+            2,
+          ]
+        }, 
+        {
+            "name":"Date Mike",
+            "photo":"https://media.giphy.com/media/ZvFsqDaaOLkSQ/giphy.gif",
+            "scores":[
+            1,
+            3,
+            5,
+            1,
+            3,
+            5,
+            1,
+            3,
+            5,
+            1
+            ] 
+        }
+      , {
+         "name":"Blind Guy McSqueezy",
+        "photo":"",
+        "scores":[
+            1,
+            5,
+            1,
+            5,
+            1,
+            5,
+            1,
+            5,
+            5,
+            1
+          ]
+        }
+        // , {
       //       "name":"Blind Guy McSqueezy",
       //   "photo":"",
       //   "scores":[
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,27 +10,29 @@ module.exports = function (app) {
 
     app.post("/api/friends", function (req, res) {
     console.log(req.body)
-        for (var i = 0; i < req.body.scores.length; i++) {
-        req.body.scores[i] = parseInt(req.body.scores[i]);
+
+    var newBestie = {name: "", photo: "", difference: Infinity}
+    var lonelyPerson = req.body;
+        // for (var i = 0; i < req.body.scores.length; i++) {
+        // req.body.scores[i] = parseInt(req.body.scores[i]);
             
-        }
-    // var lonelyPerson = req.body;
-        var newBestie = {name: "", photo: "", difference: 1000}
+        // }
+
 
         for (var i = 0; i < friendData.length; i++) {
+            var whoWillItBe = friendData[i];
             var diff = 0
-
-           for (var k = 0; k < friendData[i].scores.length; k++) {
-             diff += Math.abs(friendData[i].scores[k] - parseInt(req.body.scores[k]));
+           for (var k = 0; k < whoWillItBe.scores.length; k++) {
+             diff += Math.abs(parseInt(whoWillItBe.scores[k]) - parseInt(lonelyPerson.scores[k]));
                
         }
         if (diff < newBestie.difference) {
-            newBestie.name = friendData[i].name
-            newBestie.photo = friendData[i].photo
+            newBestie.name = whoWillItBe.name
+            newBestie.photo = whoWillItBe.photo
             newBestie.difference = diff
         }
    }
-        friendData.push(req.body);
+        friendData.push(lonelyPerson);
             res.json(newBestie);
 
     });
